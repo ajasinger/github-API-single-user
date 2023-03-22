f10a43c174f86f05602436b6f88d880db07ac51a
Aja Singer <ajasinger@gmail.com>
2023-03-22 17:19:17 -0400

    show search results

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ form.addEventListener("submit", eventHandler)
 
      //remove spaces from search value
      let nameString = search.split(' ').join('');
+
+     //allows consecutive searches 
+     document.getElementById("result").innerHTML = "";
     
      //fetch API
      fetch("https://api.github.com/users/"+nameString)
         .then((result) => result.json())
         .then((data) => {
             console.log(data);
+            //result is clickable user profile image that links to their github profile
+            document.getElementById("result").innerHTML = 
+                `<a target="_blank" href="https://www.github.com/${nameString}"> <img src="${data.avatar_url}"/></a>`
         })
 
 }
